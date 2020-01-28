4eb511a404442c7520b8cd5a85158bb8509d432a
Martin O. Pollard <mp15@sanger.ac.uk>
2020-01-28 13:40:49 +0000

    Add checked fgets to utils.cpp

--- src/utils.h
+++ src/utils.h
@@ -97,6 +97,7 @@ extern "C" {
         ATTRIBUTE((format(printf, 1, 2)));
 	int err_fputc(int c, FILE *stream);
 #define err_putchar(C) err_fputc((C), stdout)
+	char* err_fgets(char *str, int size, FILE *stream);
 	int err_fputs(const char *s, FILE *stream);
 	int err_puts(const char *s);
 	int err_fflush(FILE *stream);
